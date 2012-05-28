00d66cbb9efd98481bc93b326b3c90dc06d2494e
Administrador <Administrador@laborato-8bb70f.localdomain>
2012-05-28 17:12:02 +0200

    Support windows in the activate.py

--- deploy.py
+++ deploy.py
@@ -1,7 +1,16 @@
-activate_this = 'env/bin/activate_this.py'
+import os
+import sys
+
+if os.path.exists("env/bin"):
+    activate_this = 'env/bin/activate_this.py'
+elif os.path.exists(r"env\Scripts"):
+    activate_this = r'env\Scripts\activate_this.py'
+else:
+    print >> sys.stderr, "Error: env not found"
+    sys.exit(-1)
+    
 execfile(activate_this, dict(__file__=activate_this))
 
-import sys
 import getpass
 from optparse import OptionParser
 
--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-activate_this = 'env/bin/activate_this.py'
+import os
+import sys
+
+if os.path.exists("env/bin"):
+    activate_this = 'env/bin/activate_this.py'
+elif os.path.exists(r"env\Scripts"):
+    activate_this = r'env\Scripts\activate_this.py'
+else:
+    print >> sys.stderr, "Error: env not found"
+    sys.exit(-1)
+    
 execfile(activate_this, dict(__file__=activate_this))
 
 from labmanager.server import run
