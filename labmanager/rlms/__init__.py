e9dfe482f83d603b1db792adf56b9d2da5696259
Pablo Orduña <pablo.orduna@deusto.es>
2012-06-06 18:37:42 +0200

    Add licenses in all the documents

--- labmanager/rlms/__init__.py
+++ labmanager/rlms/__init__.py
@@ -1,3 +1,16 @@
+# -*-*- encoding: utf-8 -*-*-
+# 
+# lms4labs is free software: you can redistribute it and/or modify
+# it under the terms of the BSD 2-Clause License
+# lms4labs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+"""
+  :copyright: 2012 Pablo Orduña, Elio San Cristobal, Alberto Pesquera Martín
+  :license: BSD, see LICENSE for more details
+"""
+
 # 
 # Add the proper managers by pointing to a module
 # 
