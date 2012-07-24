50f6a0d1c4a9af3ab328a5b3a824d112dc6f42b4
Pablo Orduña <pablo.orduna@deusto.es>
2012-07-24 15:07:51 +0200

    Apply configuration in labmanager.server

--- labmanager/server.py
+++ labmanager/server.py
@@ -18,6 +18,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config.from_object('config')
 
 from labmanager.database import db_session
 
@@ -29,7 +30,6 @@ import labmanager.views
 assert labmanager.views != None # Avoid pyflakes warnings
 
 def run():
-    app.config.from_object('config')
     app.run(threaded = True, host = '0.0.0.0')
 
 if __name__ == "__main__":
