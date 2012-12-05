b68edaae77a5c94c18e3f3d759e89c9b60c7c11e
Sergio Botero <sergiobuj@gmail.com>
2012-12-05 14:22:35 -0500

    Create course when new requests
    When a request for tool comes from an unknown course (from a known LMS)
    the course gets created

--- labmanager/views/lms.py
+++ labmanager/views/lms.py
@@ -29,7 +29,8 @@ from flask import Response, render_template, request, g, abort
 # LabManager imports
 #
 from labmanager.database import db_session
-from labmanager.models   import LMS, PermissionOnLaboratory, RLMS, Laboratory, NewLMS, Credential, NewRLMS, Permission, Experiment
+from labmanager.models   import LMS, PermissionOnLaboratory, RLMS, Laboratory
+from labmanager.models import NewLMS, Credential, NewRLMS, Permission, Experiment, NewCourse
 from labmanager.rlms     import get_manager_class
 
 from labmanager import app
@@ -86,10 +87,10 @@ def requires_lms_auth(f):
 def requests():
     """SCORM packages will perform requests to this method, which will
     interact with the permitted laboratories"""
-
+    
     if request.method == 'GET':
         return render_template("test_requests.html")
-
+    
     json_data = get_json()
 #    print "\n\n\n","\n".join(["%s = %s" % (x,y) for x,y in json_data.iteritems()]),"\n\n\n"
 
@@ -122,17 +123,12 @@ def requests():
         return messages_codes['ERROR_invalid']
 
 
-
-
-
-
-
-
     # reserving...
     db_lms = db_session.query(LMS).filter_by(lms_login = g.lms).first()
     permission_on_lab = db_session.query(PermissionOnLaboratory).filter_by(lms_id = db_lms.id, local_identifier = experiment_identifier).first()
     good_msg  = messages_codes['ERROR_no_good']
     error_msg = None
+    reservation_url = ""
     if permission_on_lab is None:
         error_msg = messages_codes['ERROR_permission']
     else:
@@ -141,47 +137,38 @@ def requests():
             if course_permission.course.course_id in courses:
                 # Let the server choose among the best possible configuration
                 courses_configurations.append(course_permission.configuration)
-        if len(courses_configurations) == 0 and not general_role:
-            error_msg = messages_codes['ERROR_enrolled']
-        else:
-            lms_configuration = permission_on_lab.configuration
-            db_laboratory   = permission_on_lab.laboratory
-            db_rlms         = db_laboratory.rlms
-            db_rlms_version = db_rlms.rlms_version
-            db_rlms_type    = db_rlms_version.rlms_type
-
-
-
-
-
-
-
-
-
-
-            ManagerClass = get_manager_class(db_rlms_type.name, db_rlms_version.version)
-            remote_laboratory = ManagerClass(db_rlms.configuration)
-            reservation_url = remote_laboratory.reserve(db_laboratory.laboratory_id, author, lms_configuration, courses_configurations, request_payload, user_agent, origin_ip, referer)
-            good_msg = messages_codes['MSG_asigned'] % (db_rlms.name, db_rlms_type.name, db_rlms_version.version, reservation_url, reservation_url)
-
-            if app.config.get('DEBUGGING_REQUESTS', True):
-                rendering_data = {
-                    'name'        : cgi.escape(complete_name),
-                    'author'      : cgi.escape(author),
-                    'lms'         : cgi.escape(g.lms),
-                    'courses'     : courses,
-                    'request'     : cgi.escape(request_payload_str),
-                    'admin'       : general_role,
-                    'json'        : cgi.escape(json.dumps(json_data)),
-                    'error_msg'   : cgi.escape(error_msg or 'no error message'),
-                    'good_msg'    : good_msg or 'no good message',
-                    }
-                return render_template('debug.html', data=rendering_data)
-
-            if error_msg is None:
-                return reservation_url
+            if len(courses_configurations) == 0 and not general_role:
+                error_msg = messages_codes['ERROR_enrolled']
             else:
-                return messages_codes['ERROR_'] % error_msg
+                lms_configuration = permission_on_lab.configuration
+                db_laboratory   = permission_on_lab.laboratory
+                db_rlms         = db_laboratory.rlms
+                db_rlms_version = db_rlms.rlms_version
+                db_rlms_type    = db_rlms_version.rlms_type
+
+                ManagerClass = get_manager_class(db_rlms_type.name, db_rlms_version.version)
+                remote_laboratory = ManagerClass(db_rlms.configuration)
+                reservation_url = remote_laboratory.reserve(db_laboratory.laboratory_id, author, lms_configuration, courses_configurations, request_payload, user_agent, origin_ip, referer)
+                good_msg = messages_codes['MSG_asigned'] % (db_rlms.name, db_rlms_type.name, db_rlms_version.version, reservation_url, reservation_url)
+                print "**-",reservation_url,"-**"
+                if app.config.get('DEBUGGING_REQUESTS', True):
+                    rendering_data = {
+                        'name'        : cgi.escape(complete_name),
+                        'author'      : cgi.escape(author),
+                        'lms'         : cgi.escape(g.lms),
+                        'courses'     : courses,
+                        'request'     : cgi.escape(request_payload_str),
+                        'admin'       : general_role,
+                        'json'        : cgi.escape(json.dumps(json_data)),
+                        'error_msg'   : cgi.escape(error_msg or 'no error message'),
+                        'good_msg'    : good_msg or 'no good message',
+                        }
+                    return render_template('debug.html', data=rendering_data)
+
+    if error_msg is None:
+        return reservation_url
+    else:
+        return messages_codes['ERROR_'] % error_msg
 
 
 @app.route("/t", methods=['GET'])
@@ -193,9 +180,11 @@ def just_for_the_fun_of_testing_app(id = None):
 
 @app.before_first_request
 def verify_credentials():
+    auth = None
 
-    consumer_key = request.form['oauth_consumer_key']
-    auth = Credential.find_by_key(consumer_key)
+    if 'oauth_consumer_key' in request.form:
+        consumer_key = request.form['oauth_consumer_key']
+        auth = Credential.find_by_key(consumer_key)
 
     if auth is None:
         abort(412)
@@ -215,13 +204,18 @@ def permission_request():
 
     lms_id = int(request.form['lms_id'])
     r_id = int(request.form['resource_id'])
-    c_id = int(request.form['context_id'])
+    c_id = request.form['context_id']
 
     exp = Experiment.find_with_id_and_rlms_id(exp_id, rlms_id)
     newlms = NewLMS.find(lms_id)
+    context = NewCourse.find_by_lms_and_context(newlms, c_id)
+    print "The context: ", context, c_id, request.form['context_label']
+    if context is None:
+        context_name = request.form['context_label']
+        context = NewCourse.new(name = context_name, lms = newlms, context_id = c_id)
 
     permission_request = Permission.find_with_params(lms=newlms,
-                                                     context_id=c_id,
+                                                     context_id=context.context_id,
                                                      resource_id=r_id)
 
     if permission_request is None:
@@ -246,24 +240,18 @@ def start_ims(experiment=None):
     # check for old requests
 
     # Cross reference information
-    req_lms_name = auth.newlms.name
-    req_lms = auth.newlms.id
-    req_course = request.form['context_label']
     current_role = Set(request.form['roles'].split(','))
-    req_course_id = request.form['context_id']
     req_course_data = request.form['lis_result_sourcedid']
-    req_course_resource_id = request.form['resource_link_id']
-    req_course_resource = request.form['resource_link_title']
 
     data = { 'user_agent' : request.user_agent,
              'experiment' : experiment,
              'origin_ip' : request.remote_addr,
-             'lms' : req_lms_name,
-             'lms_id' : req_lms,
-             'course' : req_course,
-             'course_id' : req_course_id,
-             'resource_name' : req_course_resource,
-             'resource_id' : req_course_resource_id,
+             'lms' : auth.newlms.name,
+             'lms_id' : auth.newlms.id,
+             'context_label' : request.form['context_label'],
+             'context_id' : request.form['context_id'],
+             'resource_name' : request.form['resource_link_title'],
+             'resource_id' : request.form['resource_link_id'],
              'access' : False
              }
 
@@ -271,7 +259,7 @@ def start_ims(experiment=None):
         message += printdebug(request)
         data['message'] = message
 
-    exp_access = Permission.find_with_params(lms=auth.newlms, resource_id=req_course_resource_id, context_id=req_course_id)
+    exp_access = Permission.find_with_params(lms=auth.newlms, resource_id=data['resource_id'], context_id=data['context_id'])
 
     if ('Instructor' in current_role):
 
@@ -297,8 +285,9 @@ def start_ims(experiment=None):
 
         if exp_access is not None:
             data['access'] = True
+            data['experiment'] = exp_access.experiment.name
+            data['experiment_url'] = exp_access.experiment.url
 
-        data['experiment'] = experiment
         response = render_template('lti/learner_launch_tool.html', info=data)
 
     else:
