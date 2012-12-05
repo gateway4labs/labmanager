579d52a29699ae59e6e1d4de7fd240d7588a039d
Sergio Botero <sergiobuj@gmail.com>
2012-12-05 11:29:54 -0500

    Add variables for heroku
    changes in deploy.py

--- config.py
+++ config.py
@@ -2,10 +2,42 @@ import os
 
 #
 # Flask configuration
-#
+# 
 
 DEBUG      = True
 SECRET_KEY = 'secret'
 DEBUGGING_REQUESTS = False
 
-SQLALCHEMY_ENGINE_STR = os.environ.get('DATABASE_URL')
+
+heroku = os.environ.get('HEROKU', None)
+if heroku:
+    SQLALCHEMY_ENGINE_STR = os.environ.get('DATABASE_URL')
+else:
+    
+    # 
+    # DB Configuration
+    # 
+    USERNAME = 'labmanager'
+    PASSWORD = 'labmanager'
+    HOST     = 'localhost'
+    DBNAME   = 'labmanager'
+    
+    ENGINE   = 'mysql' # or 'sqlite', 'postgresql', 'oracle'
+    
+    if ENGINE == 'mysql':
+        SQLALCHEMY_ENGINE_STR = "mysql://%s:%s@%s/%s" % (USERNAME, PASSWORD, HOST, DBNAME)
+        USE_PYMYSQL = True # or False, if MySQLdb is provided
+    elif ENGINE == 'sqlite':
+        SQLALCHEMY_ENGINE_STR = "sqlite:///%s.db" % DBNAME
+    elif ENGINE == 'postgres':
+        DRIVER = 'pg8000' # or 'psycopg2'
+        SQLALCHEMY_ENGINE_STR = "postgresql+%s://%s:%s@%s/%s" % (DRIVER, USERNAME, PASSWORD, HOST, DBNAME)
+    elif ENGINE == 'oracle':
+        SQLALCHEMY_ENGINE_STR = "oracle://%s:%S@%s/%s" % (USERNAME, PASSWORD, HOST, DBNAME)
+
+  # 
+  # Not required: if you need to use a different domain name for external users
+  # add this variable:
+  # 
+  # URL_ROOT = "http://my-domain:5000/"
+  # 
