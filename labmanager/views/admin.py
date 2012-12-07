11b668e4b3e8ff44826e2b496f2dc29eb49f67b6
Sergio Botero <sbotero@mit.edu>
2012-12-07 16:27:53 -0500

    Add select fields to create views in admin
    Using select fields across the admin interface and loading some
    values from a yaml config file

--- labmanager/views/admin.py
+++ labmanager/views/admin.py
@@ -1,29 +1,70 @@
-import sys
+from sys import modules
+
+from yaml import load as yload
+
 from flask import redirect, url_for, abort
-from wtforms import fields
+
 from flask.ext import wtf
 from flask.ext.admin import Admin, BaseView, expose, AdminIndexView
 from flask.ext.admin.model import InlineFormAdmin
 from flask.ext.admin.contrib.sqlamodel import ModelView
+
 from labmanager.models import LabManagerUser as User
 from labmanager.models import NewLMS, Permission, Experiment, NewRLMS, Credential, NewCourse
 from labmanager.database import db_session as DBS
 
+configs = yload(open('labmanager/config.yaml'))
+
+class PermissionPanel(ModelView):
+    form_columns = ('newlms','newcourse','experiment','resource_link_id','configuration','access')
+    sel_choices = [(status, status.title()) for status in configs['permission_status']]
+    form_overrides = dict(access=wtf.SelectField)
+    form_args = dict(
+        access=dict( choices=sel_choices )
+        )
+
+class LabmanagerUser(ModelView):
+
+    form_columns = ('name', 'login', 'password', 'access_level')
+    sel_choices = [(level, level.title()) for level in configs['user_access_level']]
+    form_overrides = dict(access_level=wtf.SelectField)
+    form_args = dict(
+        access_level=dict( choices=sel_choices )
+        )
+
 class CredentialForm(InlineFormAdmin):
     form_columns = ('id','kind', 'key', 'secret')
     excluded_form_fields = ('id')
-    def postprocess_form(self, form):
-        form.kind = wtf.SelectField(u'Kind', choices=[('OAuth1.0','OAuth1.0'), ('Basic', 'HTTP Basic')])
-        return form
 
-class RLMSPanel(ModelView):
-    def is_accesible(self):
-        return True
+    def postprocess_form(self, form):
+        sel_choices =  [ (x , x.title()) for x in configs['authentication_types']]
+        form.kind = wtf.SelectField(u'Kind', choices=sel_choices)
+        return form
 
 class LMSPanel(ModelView):
     inline_models = (CredentialForm(Credential),)
 
     def is_accessible(self):
+        return True
+
+class RLMSPanel(ModelView):
+    form_columns = ('kind', 'location', 'url')
+    column_exclude_list = ('version')
+    sel_choices = []
+    for ins_rlms in configs['installed_rlms']:
+        for ver in configs['installed_rlms'][ins_rlms]:
+            sel_choices.append(("%s<>%s" % (ins_rlms, ver),"%s - %s" % (ins_rlms.title(), ver)) )
+    form_overrides = dict(kind=wtf.SelectField)
+    form_args = dict(
+        kind=dict( choices=sel_choices )
+        )
+
+    def on_model_change(self, form, model):
+        rlms_ver = model.kind.split('<>')
+        model.kind, model.version = rlms_ver[0], rlms_ver[1]
+        pass
+
+    def is_accesible(self):
         return True
 
 class AdminPanel(AdminIndexView):
@@ -40,9 +81,15 @@ class AdminPanel(AdminIndexView):
     def show(self, model ,r_id):
         response = ""
         try:
-            model_class = reduce(getattr, model.split("."), sys.modules[__name__])
+            model_class = reduce(getattr, model.split("."), modules[__name__])
             info = DBS.query(model_class).filter_by(id = r_id).first()
-            response = self.render('admin/models/show.html', info=info.__dict__)
+            data = {}
+            for col in info.__table__.columns:
+                col = str(col)
+                col_name = col[col.find('.') + 1:]
+                data[col_name] = info.__dict__[col_name]
+
+            response = self.render('admin/models/show.html', info=data)
         except AttributeError:
             response = abort(404)
         return response
@@ -55,14 +102,15 @@ class AdminPanel(AdminIndexView):
 def init_admin(labmanager, db_session):
     admin = Admin(index_view = AdminPanel(), name='LabManager')
 
-    admin.add_view(ModelView(Permission, session=db_session, name='Permissions'))
+    admin.add_view(PermissionPanel(Permission, session=db_session, name='Permissions'))
 
     admin.add_view(LMSPanel(NewLMS, session=db_session, category='LMS', name='LMS'))
     admin.add_view(ModelView(NewCourse, session=db_session, category='LMS', name='Courses'))
 
-    admin.add_view(ModelView(NewRLMS, session=db_session, category='ReLMS', name='RLMS'))
+    admin.add_view(RLMSPanel(NewRLMS, session=db_session, category='ReLMS', name='RLMS'))
+
     admin.add_view(ModelView(Experiment, session=db_session, category='ReLMS', name='Experiments'))
 
-    admin.add_view(ModelView(User, session=db_session, name='Users'))
+    admin.add_view(LabmanagerUser(User, session=db_session, name='Users'))
 
     admin.init_app(labmanager)
