7d85541f1be9ea8cb72a6bcfe04382efda51d09d
Pablo Orduña <pablo.orduna@deusto.es>
2012-12-13 00:05:53 +0100

    Move weblabdeusto outside labmanager/rlms/

---
+++ labmanager/rlms/ext/__init__.py
@@ -0,0 +1,29 @@
+# -*-*- encoding: utf-8 -*-*-
+# 
+# lms4labs is free software: you can redistribute it and/or modify
+# it under the terms of the BSD 2-Clause License
+# lms4labs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+"""
+Support external plug-ins without dealing with package or directory problems.
+Doing:
+
+  from labmanager.rlms.ext.weblabdeusto import foo
+
+Will internally do:
+
+  from l4l_rlms_weblabdeusto import foo
+
+We use the flask.exthook to do this.
+"""
+
+def setup():
+    from flask.exthook import ExtensionImporter
+    importer = ExtensionImporter(['l4l_rlms_%s'], __name__)
+    importer.install()
+
+
+setup()
+del setup
