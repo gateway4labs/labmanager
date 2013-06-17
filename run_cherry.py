55e712e16ef6692c120fc6ec2bf153b836a59de8
Pablo Orduña <pablo.orduna@deusto.es>
2013-06-17 22:13:59 +0200

    Remove run_cherry

--- run_cherry.py
+++
@@ -1,8 +0,0 @@
-from cherrypy import wsgiserver
-from labmanager import app
-
-# TODO: http://docs.cherrypy.org/dev/refman/process/plugins/signalhandler.html
-
-server = wsgiserver.CherryPyWSGIServer(('0.0.0.0', 8070), app)
-
-server.start()
