0d81d3e7a85cc223eb975765675c5d462b4cb8e5
Pablo Orduña <pablo.orduna@deusto.es>
2013-08-07 00:25:15 +0200

    Support select2

--- labmanager/tests/integration/admin.py
+++ labmanager/tests/integration/admin.py
@@ -17,7 +17,8 @@ def labmanager_admin_login(testcase):
 def labmanager_admin_create_lms(testcase):
     labmanager_admin_login(testcase)
     driver = testcase.driver
-    driver.get(testcase.base_url + "/")
+
+    # Create LMS
     driver.find_element_by_link_text("LMS Management").click()
     driver.find_element_by_link_text("LMS").click()
     driver.find_element_by_link_text("Create").click()
@@ -29,6 +30,8 @@ def labmanager_admin_create_lms(testcase):
     driver.find_element_by_id("full_name").send_keys("My School")
     driver.find_element_by_id("url").clear()
     driver.find_element_by_id("url").send_keys("http://myschool.com/")
+
+    # Create LMS basic authentication
     driver.find_element_by_link_text("Add Basic Http Authentications").click()
     driver.find_element_by_id("basic_http_authentications-0-lms_login").clear()
     driver.find_element_by_id("basic_http_authentications-0-lms_login").send_keys("admin")
@@ -42,6 +45,24 @@ def labmanager_admin_create_lms(testcase):
     driver.find_element_by_id("basic_http_authentications-0-labmanager_password").send_keys("password")
     driver.find_element_by_xpath("//input[@value='Submit']").click()
 
+    # Create LMS user
+    driver.find_element_by_link_text("LMS Management").click()
+    driver.find_element_by_link_text("LMS Users").click()
+    driver.find_element_by_link_text("Create").click()
+    driver.find_element_by_id("full_name").clear()
+    driver.find_element_by_id("full_name").send_keys("Administrator")
+    driver.find_element_by_id("login").clear()
+    driver.find_element_by_id("login").send_keys("admin")
+    driver.find_element_by_id("password").clear()
+    driver.find_element_by_id("password").send_keys("password")
+
+    driver.find_element_by_xpath('//a[@class="select2-choice"]').click()
+    driver.find_element_by_xpath('//div[@class="select2-result-label" and child::text()="myschool"]').click()
+    driver.find_element_by_xpath("//input[@value='Submit']").click()
+
+
+from labmanager.tests.integration.lms import labmanager_lms_login
+
 class AdminIntegrationTestCase(IntegrationTestCase, unittest.TestCase):
     def test_login(self):
         labmanager_admin_login(self)
@@ -50,6 +71,8 @@ class AdminIntegrationTestCase(IntegrationTestCase, unittest.TestCase):
 
     def test_create_lms(self):
         labmanager_admin_create_lms(self)
+        labmanager_lms_login(self)
+
         
 
 if __name__ == '__main__':
---
+++ labmanager/tests/integration/lms.py
@@ -0,0 +1,36 @@
+import unittest
+
+from labmanager.tests.integration.base import IntegrationTestCase
+
+# Utility functions.
+
+def labmanager_lms_login(testcase, from_scratch = False):
+    if from_scratch:
+        labmanager_create_lms(testcase)
+
+    driver = testcase.driver
+    driver.find_element_by_link_text("Log out").click()
+    driver.find_element_by_link_text("Manage LMS internals").click()
+    driver.find_element_by_id("lms_select").click()
+    driver.find_element_by_xpath('//a[@class="select2-choice"]').click()
+    driver.find_element_by_xpath('//div[@class="select2-result-label" and child::text()="myschool"]').click()
+    driver.find_element_by_id("username").clear()
+    driver.find_element_by_id("username").send_keys("admin")
+    driver.find_element_by_id("password").clear()
+    driver.find_element_by_id("password").send_keys("password")
+    driver.find_element_by_css_selector("button.btn.btn-success").click()
+
+    title = driver.find_element_by_tag_name('h1')
+    testcase.assertTrue('LMS Admin Dashboard' in title.text)
+
+from labmanager.tests.integration.admin import labmanager_admin_create_lms
+
+
+class LmsIntegrationTestCase(IntegrationTestCase, unittest.TestCase):
+    def test_login_lms(self):
+        labmanager_admin_create_lms(self)
+        labmanager_lms_login(self)
+
+
+if __name__ == '__main__':
+    unittest.main()
