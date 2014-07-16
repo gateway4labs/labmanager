b2dfb7a395d1bd7d2d1ac1bd0c426d0309197ea6
Danilo Zutin <dgzutin@gmail.com>
2014-07-16 19:42:28 +0200

    Add all project files

---
+++ examples/http_plugins/dotNet/g4l_plugin/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using g4l_plugin;
+
+
+namespace g4l_plugin
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+
+            RegisterRoutes(RouteTable.Routes);
+           
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            //  Code that runs on application shutdown
+
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+
+        }
+
+        static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapPageRoute("About", "About", "~/About.aspx");
+            routes.MapPageRoute("setup", "setup", "~/setup.aspx");
+            routes.MapPageRoute("plugin", "plugin/{action}", "~/plugin.aspx");
+        }        
+
+    }
+}
---
+++ examples/http_plugins/dotNet/g4l_plugin/plugin.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="plugin.aspx.cs" Inherits="g4l_plugin.plugin" %>
+
