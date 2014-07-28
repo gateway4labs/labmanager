386037218632f8789788c29b94d9dcb86a5a74f3
antonioIrizar <antonioirizar@gmail.com>
2014-07-28 13:24:34 +0200

    finish the restructure code and we have test for login_admin, login_lms, login_ple

--- labmanager/tests/test_login_admin.py
+++ labmanager/tests/test_login_admin.py
@@ -5,9 +5,10 @@ from labmanager.tests.util import G4lTestCase
 from flask.ext.testing import TestCase
 
 
-class BaseTestLogin(G4lTestCase):
-    lt_name = 'lms'
-    lt_value = 'deusto'
+class BaseTestLogin(object):
+
+    lt_name = None
+    lt_value = None
 
     def setUp(self):
         super(BaseTestLogin, self).setUp()
@@ -25,75 +26,97 @@ class BaseTestLogin(G4lTestCase):
     def logout(self):
         return self.client.get(self.logout_path, follow_redirects=True)
 
+
 class MethodsLogin(BaseTestLogin):
 
-    def test_login_admin_works(self):
+    def test_login_works(self):
         rv = self.login(username=self.username, password=self.password)
-        print session['loggeduser']
-        print "hello"
-        #self.assert_200(rv)
-        #self.assertEquals('admin', session['loggeduser'])
-        #self.assertEquals('labmanager', session['usertype'])
-    """
-    def test_login_admin_fails_wrong_form(self):
-        rv = self.login(does_not_exist="admin", password="password")
+        self.assert_200(rv)
+        self.assertEquals(self.username, session['loggeduser'])
+        self.assertEquals(self.usertype, session['usertype'])
+
+    def test_login_fails_wrong_form(self):
+        rv = self.login(does_not_exist="admin", password=self.password)
         self.assert_200(rv)
         self.assertNotIn("loggeduser", session)
 
-    def test_login_admin_fails_wrong_user(self):
-        rv = self.login(username="wrong_user", password="password")
+    def test_login_fails_wrong_user(self):
+        rv = self.login(username="wrong_user", password=self.password)
         self.assert_200(rv)
         self.assertNotIn("loggeduser", session)
 
-    def test_login_admin_fails_wrong_password(self):
+    def test_login_fails_wrong_password(self):
         rv = self.login(username="wrong_user", password="1235490295921399213")
         self.assert_200(rv)
         self.assertNotIn("loggeduser", session)
 
-    def test_login_admin_work_with_utf8(self):
-        rv = self.login(username="admin", password="utf8_char_ñ")
+    def test_login_work_with_utf8(self):
+        rv = self.login(username=self.username, password="utf8_char_ñ")
         self.assert_200(rv)
         self.assertNotIn("loggeduser", session)
 
-    def test_logout_admin_work(self):
-        rv = self.login(username="admin", password="password")
+    def test_logout_work(self):
+        rv = self.login(username=self.username, password=self.password)
         self.assertIn("loggeduser", session)
         rv = self.logout()
         self.assertNotIn("loggeduser", session)
 
-    def test_logout_admin_fails(self):
+    def test_logout_fails(self):
         rv = self.logout()
         self.assert_401(rv)
 
     def test_login_work_redirect_work(self):
-        rv = self.login(username="admin", password="password", redirect=False)
-        self.assert_redirects(rv, '/admin/')
+        rv = self.login(username=self.username,
+                        password=self.password, redirect=False)
+        self.assert_redirects(rv, self.route)
 
     def test_login_fail_redirect(self):
         rv = self.login(username="wrong_user",
-                        password="password", redirect=False)
+                        password=self.password, redirect=False)
         self.assertNotEqual(rv.status_code, 302)
-        self.assertNotEqual(rv.location, "http://localhost/admin/")
-    """
-class TestLoginAdmin(MethodsLogin):
+        self.assertNotEqual(rv.location, "http://localhost"+self.route)
+
+    def test_login_lms_select_fail(self):
+        if self.login_path != '/login/admin/':
+            self.lt_value = 10000
+            rv = self.login(username=self.username, password=self.password)
+            self.assert_200(rv)
+            self.assertNotIn("loggeduser", session)
+
+
+class TestLoginAdmin(MethodsLogin, G4lTestCase):
     login_path = '/login/admin/'
     logout_path = '/logout'
     username = 'admin'
     password = 'password'
+    usertype = 'labmanager'
+    route = '/admin/'
 
-    
 
-"""
-class TestLoginLms(BaseTestLogin, G4lTestCase):
+class TestLoginLms(MethodsLogin, G4lTestCase):
     login_path = '/login/lms/'
     logout_path = '/logout'
+    username = 'admin'
+    password = 'password'
+    usertype = 'lms'
+    lt_name = 'lms'
+    """
+        Use 1 because the name have associate a number.
+        For example Deusto have id = 1
+    """
+    lt_value = 1
+    route = '/lms_admin/'
 
-"""
 
-"""
-class TestLoginPle(BaseTestLogin, G4lTestCase):
+class TestLoginPle(MethodsLogin, G4lTestCase):
     login_path = '/login/ple/'
-    logout_path = '/logout/'
-    lt_name = 'ple'
-    lt_value = 'school1'
-"""
+    logout_path = '/logout'
+    username = 'admin'
+    password = 'password'
+    usertype = 'lms'
+    lt_name = 'lms'
+    """ Use 5 because the name have associate a number.
+        For example School have id = 5
+    """
+    lt_value = 5
+    route = '/ple_admin/'
