d4c054699fb7edf068a03016351b6c5b17e8b8f9
antonioIrizar <antonioirizar@gmail.com>
2014-07-29 11:43:17 +0200

    example in test admin rmls with flash message

--- labmanager/tests/test_admin_rmls.py
+++ labmanager/tests/test_admin_rmls.py
@@ -5,7 +5,7 @@ from labmanager.tests.util import G4lTestCase
 from labmanager.tests.test_login_admin import BaseTestLogin
 from flask.ext.testing import TestCase
 
-
+msg_error = "it's posible error msg, it has change"
 class TestRegisterLabsAdmin(BaseTestLogin, G4lTestCase):
     login_path = '/login/admin/'
     logout_path = '/logout'
@@ -13,7 +13,18 @@ class TestRegisterLabsAdmin(BaseTestLogin, G4lTestCase):
     password = 'password'
     usertype = 'labmanager'
 
+
     def test_public_availability(self):
+        rv = self.login(username=self.username, password=self.password)
+        self.assert_200(rv)
+        self.assertEquals(self.username, session['loggeduser'])
+        rv = self.client.post('/admin/rlms/labs/lab/availability/public',
+                              data=dict(lab_id=1, activate=False,
+                                        public_identifier="1"),
+                              follow_redirects=True)
+        self.assert_200(rv)
+
+    def test_public_availability_fail(self):
         rv = self.login(username=self.username, password=self.password)
         self.assert_200(rv)
         self.assertEquals(self.username, session['loggeduser'])
@@ -21,4 +32,13 @@ class TestRegisterLabsAdmin(BaseTestLogin, G4lTestCase):
                               data=dict(lab_id=1, activate=False,
                                         public_identifier=""),
                               follow_redirects=True)
+        rv = self.client.post('/admin/rlms/labs/lab/availability/public',
+                              data=dict(lab_id=2, activate=False,
+                                        public_identifier=""),
+                              follow_redirects=True)
         self.assert_200(rv)
+        try:
+            #assert "Public identifier '' already exists" in rv.data
+            self.assertIn("Public identifier '' already exists", rv.data)
+        except:
+            raise msg_error
