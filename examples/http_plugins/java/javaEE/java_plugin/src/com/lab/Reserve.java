5a848c969ff43a5bc55502bf27877f8df347295d
antonioIrizar <antonioirizar@gmail.com>
2014-08-28 14:17:16 +0200

    Reservation id is now working, add test in credentials. i add class to redirect lab, but it isn't working at this moment

--- examples/http_plugins/java/javaEE/java_plugin/src/com/lab/Reserve.java
+++ examples/http_plugins/java/javaEE/java_plugin/src/com/lab/Reserve.java
@@ -8,30 +8,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class Reserve extends Lab {
+import org.json.*;
 
+
+public class Reserve extends LabBase {
+
+	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = -8196377190923725493L;
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String systemLogin = req.getParameter("system_login");
-		String systemPassword = req.getParameter("system_password");
-		String username = req.getParameter("username");
-		String backUrl = req.getParameter("back_url");
-		if (systemLogin.equals(SYSTEM_LOGIN) && systemPassword.equals(SYSTEM_PASSWORD)){
-			long now = System.currentTimeMillis();
-			String reservationId = UUID.randomUUID().toString();
-			HttpSession s = req.getSession();
-			s.setAttribute("logged", true);
-			s.setAttribute("deadline", now+SESSION_SECONDS);
-			s.setAttribute("username", username);
-			s.setAttribute("back", backUrl);
-			s.setAttribute("reservationId", reservationId);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String systemLogin = request.getParameter("system_login");
+		String systemPassword = request.getParameter("system_password");
+		String username = request.getParameter("username");
+		String backUrl = request.getParameter("back_url");
+		JSONObject myJson = new JSONObject();
+		if (systemLogin != null && systemPassword != null){
+			if (systemLogin.equals(SYSTEM_LOGIN) && systemPassword.equals(SYSTEM_PASSWORD)){
+				long now = System.currentTimeMillis();
+				String reservationId = UUID.randomUUID().toString();
+				HttpSession s = request.getSession();
+				s.setAttribute("logged", true);
+				s.setAttribute("deadline", now + SESSION_SECONDS);
+				s.setAttribute("username", username);
+				s.setAttribute("back", backUrl);
+				s.setAttribute("reservationId", reservationId);
+				
+				myJson.put("reservation_id", reservationId);
+				myJson.put("url","http://" + request.getServerName()  +":" + request.getServerPort() + request.getContextPath() + "?reservation_id=" + reservationId);
+				response.getWriter().write(myJson.toString());
+			}		
+		}else{ 
+			myJson.put("Error", "invalid credentials'");
+			response.getWriter().write(myJson.toString());
 		}
 	}
-
 }
 
