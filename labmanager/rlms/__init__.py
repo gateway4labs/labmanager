8b9ae056ad4dd836ec6fa1c20328d128105bb65b
Pablo Orduña <pablo.orduna@deusto.es>
2016-10-25 12:06:20 -0700

    Add widgets to the labmanager

--- labmanager/rlms/__init__.py
+++ labmanager/rlms/__init__.py
@@ -53,14 +53,16 @@ _LOCAL_PERIODIC_TASKS = [
 
 
 class Laboratory(object):
-    def __init__(self, name, laboratory_id, description = None, autoload = False):
+    def __init__(self, name, laboratory_id, description=None, autoload=False, age_ranges=None, domains=None, **kwargs):
         self.name          = name
         self.laboratory_id = laboratory_id
         self.description   = description
         self.autoload      = autoload
+        self.age_ranges    = age_ranges
+        self.domains       = domains
 
     def __repr__(self):
-        return "Laboratory(%r, %r, %r, %r)" % (self.name, self.laboratory_id, self.description, self.autoload)
+        return "Laboratory(%r, %r, %r, %r, %r, %r)" % (self.name, self.laboratory_id, self.description, self.autoload, self.age_ranges, self.domains)
 
     def __hash__(self):
         return hash(self.laboratory_id)
--- labmanager/views/siway.py
+++ labmanager/views/siway.py
@@ -10,45 +10,60 @@ siway_blueprint = Blueprint('siway', __name__)
 def index():
     return "Welcome to SiWay"
 
-def lab_to_json(lab, link):
+def lab_to_json(lab, widgets):
     age_ranges = [] # e.g., 12-13, 14-15
     age_ranges = ['12-14', '14-16', '>18'] 
     domains = ['physics', 'chemistry'] # e.g., Physics, Chemistry
+    lab_widgets = []
+    for widget in widgets:
+        lab_widgets.append({
+            'app_url': widget['link'],
+            'app_title': widget['name'],
+        })
     return {
             'title': lab.name,
-            'description': "This is a sample resource",
+            'description': lab.description or '',
             'domains' : domains,
             'age_range' : age_ranges,
-            'lab_apps' : [
-                {
-					'app_url': link,
-					'app_title': lab.name,
-				}
-            ],
+            'lab_apps' : lab_widgets,
         }
+
+def _extract_labs(rlms, single_lab = None):
+    RLMS_CLASS = get_manager_class(rlms.kind, rlms.version, rlms.id)
+    rlms_inst = RLMS_CLASS(rlms.configuration)
+    labs = rlms_inst.get_laboratories()
+    public_laboratories = []
+    for lab in labs:
+        if single_lab is not None and lab.laboratory_id != single_lab.laboratory_id:
+            # If filtering, remove those labs
+            continue
+
+        if Capabilities.WIDGET in rlms_inst.get_capabilities():
+            widgets = rlms_inst.list_widgets(lab.laboratory_id)
+        else:
+            widgets = [ { 'name' : lab.name, 'description' : lab.description } ]
+
+        lab_widgets = []
+        for widget in widgets:
+            link = url_for('opensocial.public_rlms_widget_xml', rlms_identifier=rlms.public_identifier, lab_name=lab.laboratory_id, widget_name = widget['name'], _external=True)
+            lab_widgets.append({
+                'name': widget['name'],
+                'description': widget['description'],
+                'link': link,
+            })
+
+        public_laboratories.append(lab_to_json(lab, lab_widgets))
+    return public_laboratories
 
 @siway_blueprint.route('/metadata.json')
 def resources():
     public_laboratories = []
     for lab in db.session.query(Laboratory).filter_by(publicly_available = True):
-        # TODO: default
-        link = url_for('opensocial.public_widget_xml', lab_name = lab.public_identifier, widget_name = 'default', _external=True)
-        public_laboratories.append(lab_to_json(lab, link))
-    
+        for public_lab in _extract_labs(rlms, lab):
+            public_laboratories.append(public_lab)
+   
     for rlms in db.session.query(RLMS).filter_by(publicly_available = True):
-        # TODO: use this
-        RLMS_CLASS = get_manager_class(rlms.kind, rlms.version, rlms.id)
-        rlms_inst = RLMS_CLASS(rlms.configuration)
-        labs = rlms_inst.get_laboratories()
-        for lab in labs:
-            if Capabilities.WIDGET in rlms_inst.get_capabilities():
-                widgets = rlms_inst.list_widgets(lab.laboratory_id)
-            else:
-                widgets = [ { 'name' : 'lab', 'description' : 'Main view of the laboratory' } ]
-
-        for lab in rlms.laboratories:
-            # TODO: use the other approach (above)
-            link = url_for('opensocial.public_rlms_widget_xml', rlms_identifier=rlms.public_identifier, lab_name='example', widget_name = 'default', _external=True)
-            public_laboratories.append(lab_to_json(lab, link))
+        for public_lab in _extract_labs(rlms):
+            public_laboratories.append(public_lab)
 
     return jsonify(resources=public_laboratories)
