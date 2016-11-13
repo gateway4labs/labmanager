d642bd1eef9bbc4550338e39260b9925860af871
Pablo Orduña <pablo.orduna@deusto.es>
2016-11-13 10:45:41 -0800

    Improve the support of XML

--- labmanager/views/repository.py
+++ labmanager/views/repository.py
@@ -29,7 +29,7 @@ def requires_lms_auth():
 def index():
     return "Welcome to the repository"
 
-def lab_to_json(lab, widgets, fmt):
+def lab_to_json(lab, widgets):
     age_ranges = lab.age_ranges or [] # e.g., 12-13, 14-15
     domains = lab.domains or [] # e.g., Physics, Chemistry
     lab_widgets = []
@@ -63,8 +63,55 @@ def app_to_json(embed_app):
             'keywords' : []
         }
 
+def lab_to_xml(lab, widgets):
+    age_ranges = lab.age_ranges or [] # e.g., 12-13, 14-15
+    domains = lab.domains or [] # e.g., Physics, Chemistry
+    lab_widgets = []
+    for widget in widgets:
+        lab_widgets.append({
+            'labApp' : {
+                'appUrl': widget['link'],
+                'appTitle': widget['name'],
+            }
+        })
+    structure = {
+            'title': lab.name,
+            'description': lab.description or '',
+            'domains' : { 'domain': domains },
+            'ageRanges' : { 'ageRange': age_ranges },
+            'labApps' : lab_widgets,
+        }
+    if lab.keywords:
+        structure['keywords'] = { 'keyword': lab.keywords }
+    else:
+        structure['keywords'] = {}
+    return structure
 
-def _extract_labs(rlms, single_lab = None):
+
+def app_to_xml(embed_app):
+    age_ranges = embed_app.age_ranges # e.g., 12-13, 14-15
+    domains = embed_app.domains # e.g., Physics, Chemistry
+    lab_widgets = [{
+        'labApp' : {
+        'appUrl': url_for('embed.app_xml', identifier=embed_app.identifier, _external=True),
+        'appTitle': embed_app.name,
+        }
+    }]
+    return {
+            'title': embed_app.name,
+            'description': embed_app.description or '',
+            'domains' : { 'domain': domains},
+            'ageRanges' : { 'ageRange': age_ranges },
+            'labApps' : lab_widgets,
+            'keywords' : {}
+        }
+
+
+def _extract_labs(rlms, single_lab = None, fmt='json'):
+    if fmt == 'xml':
+        lab_formatter = lab_to_xml
+    else:
+        lab_formatter = lab_to_json
     RLMS_CLASS = get_manager_class(rlms.kind, rlms.version, rlms.id)
     rlms_inst = RLMS_CLASS(rlms.configuration)
     labs = rlms_inst.get_laboratories()
@@ -88,32 +135,37 @@ def _extract_labs(rlms, single_lab = None):
                 'link': link,
             })
 
-        public_laboratories.append(lab_to_json(lab, lab_widgets))
+        public_laboratories.append(lab_formatter(lab, lab_widgets))
     return public_laboratories
 
-def _get_resources():
+def _get_resources(fmt = 'json'):
     public_laboratories = []
     for lab in db.session.query(Laboratory).filter_by(publicly_available = True):
-        for public_lab in _extract_labs(rlms, lab):
+        for public_lab in _extract_labs(rlms, lab, fmt=fmt):
             public_laboratories.append(public_lab)
    
     for rlms in db.session.query(RLMS).filter_by(publicly_available = True):
-        for public_lab in _extract_labs(rlms):
+        for public_lab in _extract_labs(rlms, fmt=fmt):
             public_laboratories.append(public_lab)
 
+    if fmt == 'xml':
+        app_formatter = app_to_xml
+    else:
+        app_formatter = app_to_json
+
     for app in db.session.query(EmbedApplication).all():
-        public_laboratories.append(app_to_json(app))
+        public_laboratories.append(app_formatter(app))
 
     return public_laboratories
 
 @repository_blueprint.route('/metadata.json')
 def resources():
-    public_laboratories = _get_resources()
+    public_laboratories = _get_resources(fmt='json')
     return jsonify(resources=public_laboratories)
 
 @repository_blueprint.route('/metadata.xml')
 def resources_xml():
-    public_laboratories = _get_resources()
+    public_laboratories = _get_resources(fmt='xml')
     return Response(dict2xml({
         "resources": {
             "resource" : public_laboratories
